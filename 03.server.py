d385da87827bd1ae219a11662758cf1145a198b1
Gowrishankar P <atmegarobotics@gmail.com>
2021-12-07 09:53:53 +0530

    simple server

---
+++ 03.server.py
@@ -0,0 +1,15 @@
+import socket 
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+port = 12345 
+s.bind(('', port))
+print("port binded to :",port)
+s.listen(5)
+print("socket is listening")
+while True:
+    c, addr = s.accept()
+    print('Got connectiom from ', addr)
+    c.send("thank you for connecting".encode())
+    c.close()
+    break 
