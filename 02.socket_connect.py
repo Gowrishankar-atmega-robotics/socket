34e7b4fc804c1b20aaefef005e476925f1da429a
Gowrishankar P <atmegarobotics@gmail.com>
2021-12-07 09:40:13 +0530

    socket_google_connect

---
+++ 02.socket_connect.py
@@ -0,0 +1,9 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+port  = 80
+
+host_ip = socket.gethostbyname('www.atmegarobotics.tech.blog')
+
+s.connect((host_ip, port))
+print("connected success")
